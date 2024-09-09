f0137c698d3b34e829b0b00900ace2b42fad2460
herinpatel15 <herinpatel15113@gmail.com>
2024-09-09 15:20:10 +0530

    lic-1

--- packages/ui/.eslintrc.js
+++ packages/ui/.eslintrc.js
@@ -1,7 +1,7 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
-  extends: ["@ui-particle/eslint-config/react-internal.js"],
+  extends: ["paramanu/eslint-config/react-internal.js"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: "./tsconfig.lint.json",
